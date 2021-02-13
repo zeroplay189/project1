591416b2b722fe3f7489bb22de513c004e556c09
zeroplay189 <78993174+zeroplay189@users.noreply.github.com>
2021-02-13 15:22:56 +0700

    ๊User
    สร้างตารางให้อยู่ใน Database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->name = "Surachet";
+        $user->username="admin";
+        $user->email ="61123855@g.cmru.ac.th";
+        $user->password = Hash::make("1234");
+        $user->save();
     }
 }
