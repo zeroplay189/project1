805753f172f21608398a7aa1d6937572dd1af23e
zeroplay189 <78993174+zeroplay189@users.noreply.github.com>
2021-02-13 14:28:46 +0700

    Masterpage
    บันทึกใช้หลายๆหน้า

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-   <a href="/">หน้าแรก</a> |<a href="/about">เกี่ยวกับ</a> | <a href="/contact">ติดต่อ</a>
+@extends('layout.master')
+@section('content')
     <h1>สวัสดี  laravel</h1>
-</body>
-</html>
+@endsection
